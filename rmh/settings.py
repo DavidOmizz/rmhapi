9632f097c7e7626fba45cd8f134358d7737696c6
David Omisakin <davidomisakin4good@gmail.com>
2024-08-26 00:14:26 +0100

    Initial commit2

--- rmh/settings.py
+++ rmh/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7##o1xm-o$%ym+dv%cpv2eg68+oto$6bppapntk&l3r1ad))u-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
